89e7e8f81d70b96e96a796b6832eff2a491a8504
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-20 21:27:33 +0000

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import Popular from './components/popular';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Popular from './components/Popular';
+import Battle from './components/Battle';
+import Home from './components/Home';
+import Nav from './components/Nav';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Popular />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/battle" component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route render={ () => {
+              return (
+                <p>Not Found</p>
+              )
+            }} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Battle.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Battle extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Battle</h1>
+      </div>
+    )
+  }
+}
+
+export default Battle;
